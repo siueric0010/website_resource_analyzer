bf3233fb5886f9c750cd2ff6f26e2796726a932c
Eric Siu <siueric0010@gmail.com>
2019-11-09 18:24:32 -0800

    added webRequest inside peermissons, might remove later bc not working and planning top continue with declarative COntent

--- background.js
+++ background.js
@@ -1,5 +1,5 @@
 chrome.runtime.onInstalled.addListener(function() {
-    alert('Installed.');
+    alert('Installed/Updated.');
     chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
       chrome.declarativeContent.onPageChanged.addRules([rule1]);
     });
@@ -7,10 +7,17 @@ chrome.runtime.onInstalled.addListener(function() {
 
 // define rule for button to be active
 var rule1 = {
+    
+    // Extension is on (i.e. pressing the icon will show a button w/ the URL, if the URL contains a ".")
     conditions: [
+
         new chrome.declarativeContent.PageStateMatcher({
             pageUrl: {hostContains: '.'},
         })
     ],
     actions: [new chrome.declarativeContent.ShowPageAction()]
 };
+
+chrome.webRequest.onBeforeRequest.addListener(function(details) {
+    alert('test');
+});
--- popup.js
+++ popup.js
@@ -1,5 +1,13 @@
+// openWindow refers to the button inside the window which is opened by pressing the icon on a valid website with "." in its url
 let openWindow = document.getElementById('openWindow');
 
+// Check if the tab is the current tab, if so then set the buttontext to be the url
 chrome.tabs.query({active: true, currentWindow:true}, function(tabs) {
-    openWindow.innerText = tabs[0].url
+    openWindow.innerText = extractDomain(tabs[0].url);
 });
+
+// Grabs the domain + subdomains from URL (after the first // and before the first /)
+function extractDomain(url) {
+    return url.split("//")[1].split("/")[0];
+}
+
