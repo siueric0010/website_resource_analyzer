afd67481f545a3e87d6a063cd5383ca385a4e936
Eric Siu <siueric0010@gmail.com>
2019-11-18 16:36:16 -0800

    made it possible to count domain, but does not work consistently

--- background.js
+++ background.js
@@ -1,6 +1,8 @@
-chrome.runtime.onInstalled.addListener(function() {
-    alert('Installed/Updated.');
+var requestUrls = [];
+var domainCounter = 0;
+var oldUrl = "";
 
+chrome.runtime.onInstalled.addListener(function() {
 
     // define rule for button to be active
     var rule = {
@@ -23,16 +25,46 @@ chrome.runtime.onInstalled.addListener(function() {
 
 
 chrome.webRequest.onCompleted.addListener(function(details) {
+
+    // If the webrequest is just my extension or undefined, do not count it
+    if(details.url.includes(chrome.runtime.id) || details.url === null|| details.url === undefined || typeof details === 'undefined') return;
+
+    // Count all the requests and keep track of its domains
+    var urlDomain = extractDomain(details.url);
+
+    // If the domain is present, do not count it
+    if(requestUrls.includes(urlDomain)) return;
+    requestUrls.push(urlDomain);
+    domainCounter++;
     // Works: alert('test', details);
-    let listOfDomains = document.getElementById("listOfDomains"); // returns null, must fix
+    //let listOfDomains = document.getElementById("listOfDomains"); // returns null, must fix
     // listOfDomains.textContent += extractDomain(details.url);
+    chrome.storage.local.set({'info': requestUrls});
+    if(domainCounter != 0) {
+        chrome.storage.local.set({'domainCounter': domainCounter});
+    }
 } , {urls: [ "<all_urls>" ]},[]);
 
+chrome.tabs.onCreated.addListener(function(tabid, changeinfo, tab) {
+    requestUrls = [];
+    domainCounter = 0;
+});
 
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+    if(oldUrl !== tab.url) {
+        domainCounter = 0;
+      oldUrl = tab.url;
+    }
+  });
 // // TODO: create a rule for getting web requests and acting on it
 // var rule2 = {
 //     conditions: [
 //      //   new chrome.declarativeContent.webRequest
 //     ]
 // }
+
+// Grabs the domain + subdomains from URL (after the first // and before the first /)
+function extractDomain(url) {
+    return url.split("//")[1].split("/")[0];
+}
 
--- popup.js
+++ popup.js
@@ -1,9 +1,18 @@
 // openWindow refers to the button inside the window which is opened by pressing the icon on a valid website with "." in its url
 let openWindow = document.getElementById('openWindow');
+let listOfDomains = document.getElementById('listOfDomains');
 
 // Check if the tab is the current tab, if so then set the buttontext to be the url
 chrome.tabs.query({active: true, currentWindow:true}, function(tabs) {
     openWindow.innerText = extractDomain(tabs[0].url);
+    chrome.storage.local.get(['info', 'domainCounter'], function (result) {
+        var stringList = "List: \n";
+        for(domain of result.info) {
+            stringList += domain + "\n";
+        }
+        
+        listOfDomains.innerText = stringList + "\nDomain Count: " + result.domainCounter;
+    });
 });
 
 // Grabs the domain + subdomains from URL (after the first // and before the first /)
